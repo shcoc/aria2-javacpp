c99c99753b7a77613255f75b649a340aa101b637
zsaltedfish <1976648487@qq.com>
2022-09-21 17:09:21 +0800

    修改 包名

--- src/main/java/io/github/ssaltedfish/aria2/Aria2C.java
+++ src/main/java/io/github/ssaltedfish/aria2/Aria2C.java
@@ -1,11 +1,12 @@
-package com.zsaltedfish.aria2;
+package io.github.ssaltedfish.aria2;
 
+import io.github.ssaltedfish.aria2.prest.Aria2;
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.annotation.Platform;
 import org.bytedeco.javacpp.annotation.Properties;
 
 @Properties(
-        inherit = {com.zsaltedfish.aria2.prest.Aria2.class},
+        inherit = {Aria2.class},
         value = @Platform(executable = "aria2c")
 )
 public class Aria2C {
